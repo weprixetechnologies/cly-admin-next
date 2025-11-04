3a7946931a4edb0a1a3cf640b3a2a584e6b71776
Ronit Sarkar <rseditz57@gmail.com>
2025-11-04 23:39:50 +0530

    M

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['72.60.219.181', '127.0.0.1', 'picsum.photos', 'cly-pull.b-cdn.net', 'ithyaraa.b-cdn.net', 'cly-pull-bunny.b-cdn.net', 'example.com'],
+    },
+};
 
 export default nextConfig;
